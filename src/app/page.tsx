4bfe8f2ed57c27bc9ae6993722b219b11a92d3a7
Darek Radke <dradmclad@gmail.com>
2023-12-08 12:09:25 -0600

    home page css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,48 +36,71 @@ export default function Home() {
         <div
           onMouseEnter={handleHoverOne}
           className={`overflow-hidden z-0 w-screen ${
-            isBigOne ? "lg:transition-all ease-in-out lg:w-1/2 lg:duration-500" : "lg:transition-all ease-in-out lg:w-1/4 lg:duration-500"
+            isBigOne
+              ? "lg:transition-all ease-in-out lg:w-1/2 lg:duration-500"
+              : "lg:transition-all ease-in-out lg:w-1/4 lg:duration-500"
           }`}
         >
-          <p className="text-black rounded-br-md bg-white px-2 absolute">
-            Weddings & Events
-          </p>
-          <Image
-            src={WeddingImage}
-            alt="stars"
-            className="h-[400px] object-cover"
-          />
+          <Link href={"/portfolio/weddingsevents"}>
+            <p className={`h-[28px] mb-2 ${isBigOne ? "lg:transition-all ease-in-out lg:text-3xl lg:duration-500" : "lg:transition-all ease-in-out lg:text-xl lg:duration-500"}`}>
+              Weddings & Events
+            </p>
+            <Image
+              src={WeddingImage}
+              alt="stars"
+              className="h-[400px] object-cover"
+            />
+          </Link>
         </div>
         <div
           onMouseEnter={handleHoverTwo}
           className={`overflow-hidden ${
-            isBigTwo ? "lg:transition-all ease-in-out lg:w-1/2 lg:duration-500" : "lg:transition-all ease-in-out lg:w-1/4 lg:duration-500"
+            isBigTwo
+              ? "lg:transition-all ease-in-out lg:w-1/2 lg:duration-500"
+              : "lg:transition-all ease-in-out lg:w-1/4 lg:duration-500"
           }`}
         >
-          <p className="text-black rounded-br-md bg-white px-2 absolute">
-            Head Shots & Modeling
-          </p>
-          <Image
-            src={WomanImage}
-            alt="balloons"
-            className="h-[400px] object-cover"
-          />
+          <Link href={"/portfolio/headshots"}>
+            <p className={`h-[28px] mb-2 ${isBigTwo ? "lg:transition-all ease-in-out lg:text-3xl lg:duration-500" : "lg:transition-all ease-in-out lg:text-xl lg:duration-500"}`}>
+              Head Shots & Modeling
+            </p>
+            <Image
+              src={WomanImage}
+              alt="balloons"
+              className="h-[400px] object-cover"
+            />
+          </Link>
         </div>
         <div
           onMouseEnter={handleHoverThree}
           className={`overflow-hidden ${
-            isBigThree ? "lg:transition-all ease-in-out lg:w-1/2 lg:duration-500" : "lg:transition-all ease-in-out lg:w-1/4 lg:duration-500"
+            isBigThree
+              ? "lg:transition-all ease-in-out lg:w-1/2 lg:duration-500"
+              : "lg:transition-all ease-in-out lg:w-1/4 lg:duration-500"
           }`}
         >
-          <p className="text-black rounded-br-md bg-white px-2 absolute">
-            Family Portraits
-          </p>
-          <Image
-            src={FamilyImage}
-            alt="street"
-            className="h-[400px] object-cover"
-          />
+          <Link href={"/portfolio/familyportraits"}>
+            <p className={`h-[28px] mb-2 ${isBigThree ? "lg:transition-all ease-in-out lg:text-3xl lg:duration-500" : "lg:transition-all ease-in-out lg:text-xl lg:duration-500"}`}>
+              Family Portraits
+            </p>
+            <Image
+              src={FamilyImage}
+              alt="street"
+              className="h-[400px] object-cover"
+            />
+          </Link>
         </div>
+      </div>
+      <div className="flex max-w-[1200px] m-auto text-center justify-center">
+        <div className="relative"></div>
+        <div></div>
+        <p className="py-3 lg:py-10 lg:w-[600px]">
+          <span>A</span>t Insight photography we capture the highest quality
+          images from the best angles for any event. You never have to worry
+          about missing a precious moment. If you're looking for family
+          portraits, look no more. Take your family photos in your own home. We
+          bring the studio to you.
+        </p>
       </div>
     </>
   );
